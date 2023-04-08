23ad951c07ecbaa70e2e4e34c4b9c57638f2a38f
mike stack <mikestack15@gmail.com>
2023-04-07 22:37:15 -0500

    working docker env and functioning dag

--- airflow/dags/grab_open_weather_data.py
+++ airflow/dags/grab_open_weather_data.py
@@ -2,6 +2,27 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
 from airflow.utils.task_group import TaskGroup
+from airflow.decorators import task
+from scripts.extract_open_weather_api_to_lake import extract_open_weather_to_lake
+import requests
+import boto3
 
+with DAG(
+    'weather-data-api-extraction',
+    default_args={
+        'depends_on_past': False,
+        'retries': 0,
+    },
+    description='run extraction for weather api data from open weather map',
+    schedule_interval=None,
+    start_date=datetime(2023, 3, 31),
+    catchup=False,
+    tags=['weather-api-pipeline'],
+) as dag:
 
+    extract_data = PythonOperator(
+        task_id='extract_weather_data',
+        python_callable=extract_open_weather_to_lake,
+    )
--- airflow/dags/scripts/extract_open_weather_api_to_lake.py
+++ airflow/dags/scripts/extract_open_weather_api_to_lake.py
@@ -1,44 +1,19 @@
-import os
 from datetime import date, datetime
 import requests
 import json
+import pprint
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 
+def extract_open_weather_to_lake():
+    """
+    retrieve open weather api data via requests library and writes to correct s3 data lake endpoint
+    """
 
-def extract_open_weather_api_to_lake():
-    # retrieve open weather api data via curl command
-
-    params = {'Content'}
     url = "https://api.openweathermap.org/data/2.5/weather?lat=3.5553&lon=98.1448&appid=726baba800d0a2c82084d5bb77daa499"
     response = requests.get(url=url)
+    s3_hook = S3Hook(aws_conn_id='aws_default')
+    weather_data_content = response.json()
 
-    x = response.json()
-
-    try:
-        z = x['rain']
-    except:
-        pass
-
-
-    current_weather_summary = x['weather']
-
-    main = x['main']
-
-    current_temp_kelvin = main['temp']
-
-    current_wind = x['wind']
-
-    visibility = x['visibility']
-
-    print(current_weather_summary)
-    print(main)
-    print(current_temp_kelvin)
-    print(current_wind)
-    print(visibility)
-
-    return main
-
-# {'temp': 296.46, 'feels_like': 297.38, 'temp_min': 296.46, 'temp_max': 296.46, 'pressure': 1012, 'humidity': 97, 'sea_level': 1012, 'grnd_level': 992}
-
-
-if __name__ == "__main__":
-    extract_open_weather_api_to_lake()
+    main_weather_content = weather_data_content['main']
+    data = json.dumps(main_weather_content)
+    s3_hook.load_string(data,'main_weather_content.json',bucket_name='orangutan-orchard',replace=True)
