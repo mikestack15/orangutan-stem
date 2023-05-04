7df8dcbdc20280dfc6a939de2b7217495bf63042
mike stack <mikestack15@gmail.com>
2023-05-04 18:13:52 -0500

    adding correct s3 endpoints

--- airflow/dags/extract_open_weather_data_to_lake.py
+++ airflow/dags/extract_open_weather_data_to_lake.py
@@ -3,9 +3,32 @@ from airflow.utils.dates import days_ago
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 import requests
 import json
+import datetime
 
 
-@dag(start_date=days_ago(2),
+# set date variables for f string in data lake load
+dt = datetime.datetime.now()
+current_year = dt.strftime('%Y')
+current_month = dt.strftime('%m')
+current_day = dt.strftime('%d')
+current_hour = dt.strftime('%H')
+"""
+Activity 1: Open Weather Map API Airflow DAG
+
+Author: Michael Stack
+Last Updated: 5/4/2023
+
+This DAG should work both locally and server-side if you utilize the docker-compose file in the airflow directory
+
+Goal: Extract Open Weather Map API data into date partitioned s3 bucket/key path
+
+This is a good use-case of the Airflow Taskflow API
+Be sure to follow along on the wiki if you guidance on getting this dag to work on your desired machine
+
+"""
+
+
+@dag(start_date=days_ago(1),
      schedule='@hourly',
      tags=['extract','weather-data'],
      description='run extraction for weather api data from open weather map',
@@ -31,15 +54,15 @@ def extract_open_weather_data_to_lake():
     @task()
     def load(main_weather_content: dict):
         """
-        loads open weather api response (dict/json) into 
+        loads open weather api response (dict/json) into s3 bucket/folder
         Params:
         main_weather_content: dict
-        dictionary response 
+        dictionary response from main_weather_content
         """
         data = json.dumps(main_weather_content)
 
         s3_hook = S3Hook(aws_conn_id='aws_default')
-        s3_hook.load_string(data,'main_weather_content.json',bucket_name='orangutan-orchard',replace=True)
+        s3_hook.load_string(data,f'open_weather_map/bukit_lawang/{current_year}/{current_month}/{current_day}/main_weather_content.json',bucket_name='orangutan-orchard',replace=True)
 
 
     main_weather_data = extract()
