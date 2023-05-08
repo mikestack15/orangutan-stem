80180ca9893370222c0509e6b0226c6a11013af8
mike stack <mikestack15@gmail.com>
2023-05-08 13:17:54 -0500

    updated dag for weather api

--- airflow/dags/extract_open_weather_data_to_lake.py
+++ airflow/dags/extract_open_weather_data_to_lake.py
@@ -4,26 +4,29 @@ from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 import requests
 import json
 import datetime
+import os
 
 
 # set date variables for f string in data lake load
 dt = datetime.datetime.now()
-current_year = dt.strftime('%Y')
-current_month = dt.strftime('%m')
-current_day = dt.strftime('%d')
-current_hour = dt.strftime('%H')
+current_year, current_month, current_day, current_hour = dt.strftime('%Y'), dt.strftime('%m'), dt.strftime('%d'), dt.strftime('%H')
+
+# set environment variables for open weather map api
+open_weather_api_key = os.environ['open_weather_api_key']
+
 """
 Activity 1: Open Weather Map API Airflow DAG
 
 Author: Michael Stack
-Last Updated: 5/4/2023
+Last Updated: 5/5/2023
 
-This DAG should work both locally and server-side if you utilize the docker-compose file in the airflow directory
+This DAG should work both locally and server-side if you utilize the docker-compose file in the airflow directory,
+and have the open weather map api key set as an environment variable.
 
-Goal: Extract Open Weather Map API data into date partitioned s3 bucket/key path
+Goal: Extract Open Weather Map API data into a date partitioned s3 bucket/key path
 
 This is a good use-case of the Airflow Taskflow API
-Be sure to follow along on the wiki if you guidance on getting this dag to work on your desired machine
+Be sure to follow along on the wiki if you need guidance on getting this dag to work on your desired machine
 
 """
 
@@ -44,8 +47,7 @@ def extract_open_weather_data_to_lake():
         main_weather_content: dict
         """
 
-        url = "https://api.openweathermap.org/data/2.5/weather?lat=3.5553&lon=98.1448&appid=726baba800d0a2c82084d5bb77daa499"
-        response = requests.get(url=url)
+        response = requests.get(url=f"https://api.openweathermap.org/data/2.5/weather?lat=3.5553&lon=98.1448&appid={open_weather_api_key}")
         weather_data_content = response.json()
         main_weather_content = weather_data_content['main']
 
@@ -62,7 +64,8 @@ def extract_open_weather_data_to_lake():
         data = json.dumps(main_weather_content)
 
         s3_hook = S3Hook(aws_conn_id='aws_default')
-        s3_hook.load_string(data,f'raw/open_weather_map/bukit_lawang/{current_year}/{current_month}/{current_day}/main_weather_content.json',bucket_name='orangutan-orchard',replace=True)
+        s3_hook.load_string(data,f'raw/open_weather_map/bukit_lawang/{current_year}/{current_month}/{current_day}/{current_hour}/ \
+                            main_weather_content.json',bucket_name='orangutan-orchard',replace=True)
 
 
     main_weather_data = extract()
